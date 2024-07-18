8f5a9ddbaf1f43c302e3bf3ef5b968b42ca8b90b
Marino Gil <gilmarinoj@gmail.com>
2024-07-18 12:14:03 -0400

    added CreateCarDto

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './dtos/create-car.dto';
+
+@Injectable()
+export class CarsService {
+
+    private cars: Car[] = [
+        {
+            id: uuid(),
+            brand: 'Toyota',
+            model: 'Corolla',
+        },
+        {
+            id: uuid(),
+            brand: 'Honda',
+            model: 'Civic',
+        },
+        {
+            id: uuid(),
+            brand: 'Chevrolet',
+            model: 'Spark',
+        }
+    ]
+
+    findAll() {
+        return this.cars;
+    }
+
+    findOneById(id: string) {
+        const car = this.cars.find(car => car.id === id)
+        if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+        return car;
+    }
+
+    create(createCarDto: CreateCarDto) {
+        const car: Car = {
+            id: uuid(),
+            ...createCarDto
+        }
+        this.cars.push( car );
+
+        return car;
+    }
+}
